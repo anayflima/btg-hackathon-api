6be40e433a31c05d9abc9ec1073feb3e18542b30
anayflima <ayf.lima@usp.br>
2022-04-03 14:58:25 -0300

    Add necessary libraries in install script

--- install.py
+++ install.py
@@ -2,16 +2,15 @@ import sys
 import subprocess
 
 # implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'Flask'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'requests'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install',
-'pandas'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Flask'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'requests'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'python-math'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'python-math'])
 
 # process output with an API in the subprocess module:
-reqs = subprocess.check_output([sys.executable, '-m', 'pip',
-'freeze'])
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
 installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 print(installed_packages)
