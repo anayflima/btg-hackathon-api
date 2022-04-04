c84e9317c983b5250dd918cb0544908ca415cfc6
anayflima <ayf.lima@usp.br>
2022-04-03 22:20:18 -0300

    Add versions of libraries in install script

--- install.py
+++ install.py
@@ -8,6 +8,9 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pandas'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'python-math'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'flask-cors'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'werkzeug==0.16.1'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'jinja2==3.0.1'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'itsdangerous==2.0.1'])
 
 # process output with an API in the subprocess module:
 reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
